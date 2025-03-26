4cf208ba544bf71f5eadd40178bb0424ab7bce2f
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-26 09:56:14 -0300

    chore: fix dependencies

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById('app'));
+import App from './main/App'
+
+ReactDOM.render(<App />, document.getElementById('app'))
--- Frontend/src/main/Routes.jsx
+++ Frontend/src/main/Routes.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import { Router, Route, IndexRedirect, hashHistory } from 'react-router';
+import React from 'react'
+import { Route, Redirect } from 'react-router'
+import { HashRouter } from 'react-router-dom';
 
-import Todo from '../todo/Todo';
-import About from '../about/About';
-import App from './App'; // Se tiver um componente principal
+import Todo from '../todo/Todo'
+import About from '../about/About'
 
-const Routes = () => (
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRedirect to="/todos" />
-      <Route path="todos" component={Todo} />
-      <Route path="about" component={About} />
-    </Route>
-  </Router>
-);
+const Routes = props => (
+    <HashRouter>
+        <Redirect from='*' to='/todos' />
+        <Route path='/todos' component={Todo} />
+        <Route path='/about' component={About} />
+    </HashRouter>
+)
 
-export default Routes;
+export default Routes
