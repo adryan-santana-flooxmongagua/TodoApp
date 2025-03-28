dcc104fd7b3a9cf2cbc5578f31e758efddcbe7d7
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-28 10:43:59 -0300

    feat: connect Todoform with Redux

--- Front-end/src/todo/TodoForm.jsx
+++ Front-end/src/todo/TodoForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Grid from '../template/Grid'
 import IconButton from '../template/IconButton'
 
@@ -27,4 +28,5 @@ const TodoForm = props => {
     )
 }
 
-export default TodoForm
+const mapStateToProps = state => ({description: state.todo.description})
+export default connect(mapStateToProps)(TodoForm)
